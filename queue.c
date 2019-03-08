df3a02fe5a39a959e43f290d013095940cd9405f
Ray Lin <fishnbg@gmail.com>
2019-03-09 01:33:05 +0800

    Complete all functions

--- queue.c
+++ queue.c
@@ -24,9 +24,16 @@
 */
 queue_t *q_new()
 {
-    queue_t *q = malloc(sizeof(queue_t));
+    queue_t *q = (queue_t *) malloc(sizeof(queue_t));
     /* What if malloc returned NULL? */
+    if (q == NULL) {
+        perror("Fail to malloc for queue_t\n");
+        return NULL;
+    }
+
     q->head = NULL;
+    q->tail = NULL;
+    q->size = 0;
     return q;
 }
 
@@ -35,6 +42,17 @@ void q_free(queue_t *q)
 {
     /* How about freeing the list elements and the strings? */
     /* Free queue structure */
+    list_ele_t *curr = NULL;
+
+    if (!q)
+        return;
+
+    for (curr = q->head; curr != NULL; curr = q->head) {
+        q->head = q->head->next;
+        free(curr->value);
+        free(curr);
+    }
+
     free(q);
 }
 
@@ -49,11 +67,32 @@ bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
     /* What should you do if the q is NULL? */
+    if (!q) {
+        perror("q is NULL\n");
+        return false;
+    }
     newh = malloc(sizeof(list_ele_t));
+    if (!newh) {
+        perror("Failed to allocate memory for new element.\n");
+        return false;
+    }
+
+    newh->value = (char *) malloc(strlen(s) + 1);
+    if (!newh->value) {
+        perror("Failed to allocate memory for value of new element.\n");
+        free(newh);
+        return false;
+    }
+
+    strncpy(newh->value, s, strlen(s));
+    newh->value[strlen(s)] = '\0';
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     newh->next = q->head;
     q->head = newh;
+    if (!q->tail)
+        q->tail = newh;
+    q->size++;
     return true;
 }
 
@@ -67,9 +106,40 @@ bool q_insert_head(queue_t *q, char *s)
  */
 bool q_insert_tail(queue_t *q, char *s)
 {
+    list_ele_t *newt;
     /* You need to write the complete code for this function */
     /* Remember: It should operate in O(1) time */
-    return false;
+    if (!q) {
+        perror("q is NULL\n");
+        return false;
+    }
+
+    newt = malloc(sizeof(list_ele_t));
+    if (!newt) {
+        perror("Failed to allocate memory for new element.\n");
+        return false;
+    }
+
+    newt->value = (char *) malloc(strlen(s) + 1);
+    if (!newt->value) {
+        perror("Failed to allocate memory for value of new element.\n");
+        free(newt);
+        return false;
+    }
+
+    strncpy(newt->value, s, strlen(s));
+    newt->value[strlen(s)] = '\0';
+    newt->next = NULL;
+    if (q->tail) {
+        q->tail->next = newt;
+        q->tail = newt;
+    } else {
+        q->tail = newt;
+        q->head = newt;
+    }
+
+    q->size++;
+    return true;
 }
 
 /*
@@ -82,8 +152,36 @@ bool q_insert_tail(queue_t *q, char *s)
 */
 bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
+    list_ele_t *curr;
+
     /* You need to fix up this code. */
-    q->head = q->head->next;
+    if (!q) {
+        perror("q is NULL\n");
+        return false;
+    }
+
+    if (!q->head) {
+        perror("q is empty\n");
+        return false;
+    }
+    curr = q->head;
+    if (q->head->next)
+        q->head = q->head->next;
+    else
+        q->head = q->tail = NULL;
+
+    if (sp) {
+        size_t len = strlen(curr->value);
+        if (len >= bufsize)
+            len = bufsize - 1;
+
+        strncpy(sp, curr->value, len);
+        sp[len] = '\0';
+    }
+
+    free(curr->value);
+    free(curr);
+    q->size--;
     return true;
 }
 
@@ -95,7 +193,10 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-    return 0;
+    if (!q)
+        return 0;
+    else
+        return q->size;
 }
 
 /*
@@ -107,5 +208,25 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
+    list_ele_t *pnext, *pprev = NULL;
     /* You need to write the code for this function */
+    if (!q) {
+        perror("q is NULL\n");
+        return;
+    }
+
+    if (!q->head) {
+        perror("q is empty\n");
+        return;
+    }
+
+    pnext = q->head;
+    q->tail = q->head;
+    while (q->head) {
+        pnext = q->head->next;
+        q->head->next = pprev;
+        pprev = q->head;
+        q->head = pnext;
+    }
+    q->head = pprev;
 }
